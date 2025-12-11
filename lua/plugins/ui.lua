b07dde2ca4af5e2815b48f1ebc766c9e7c23e9fb
Skye <2974273572@qq.com>
2025-12-11 16:00:07 +0800

    2025年12月11日更新

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -159,22 +159,22 @@ return {
 
 -- 添加 which-key.nvim 插件
 {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
-  keys = {
-    {
-      "<leader>?",
-      function()
-        require("which-key").show({ global = false })
-      end,
-      desc = "Buffer Local Keymaps (which-key)",
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
     },
-  },
+    keys = {
+        {
+            "<leader>?",
+            function()
+                require("which-key").show({ global = false })
+            end,
+            desc = "Buffer Local Keymaps (which-key)",
+        },
+    },
 }
 
 }
