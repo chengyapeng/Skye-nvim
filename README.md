24e0e8c4defcc10670426a8ecc0969aeb8173927
Skye <2974273572@qq.com>
2025-12-11 11:09:36 +0800

    2025年12月11日更新

---
+++ README.md
@@ -0,0 +1,3 @@
+## 自用配置
+在 lsp.lua 文件中加入 "folke/lazydev.nvim" 插件。
+
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -155,6 +155,26 @@ return {
             { filter = { event = "msg_show", kind = "", }, opts = { skip = true }, },
         },
     }
+},
+
+-- 添加 which-key.nvim 插件
+{
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
 }
 
 }
